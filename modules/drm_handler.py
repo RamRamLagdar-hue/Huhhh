4486f3ea4314b09a46ee57f4ccb27e9bfaa2760e
RamRamLagdar-hue <ankitgehlot120021@gmail.com>
2026-04-25 23:15:47 +0530

    Update drm_handler.py

--- modules/drm_handler.py
+++ modules/drm_handler.py
@@ -427,7 +427,9 @@ async def drm_handler(bot: Client, m: Message):
 
             # --- YE NAYI LINES ADD KARO ---
             elif "cloudfront.net" in url:
-                cmd = f'yt-dlp -o "{name}.mp4" --external-downloader aria2c --external-downloader-args "aria2c:-x 16 -s 16 -k 1M" "{url}"'
+                # --remux-video mp4 use karo taaki extension hamesha fix rahe
+                cmd = f'yt-dlp -o "{name}.mp4" --remux-video mp4 --external-downloader aria2c --external-downloader-args "aria2c:-x 16 -s 16 -k 1M" "{url}"'
+                filename = f"{name}.mp4
 
 
             else:
